534436025a55885c30ddb0a2d10c3031c2cf81cc
R. Scott Bradshaw <scottbradshaw@nc.rr.com>
2015-07-27 12:01:18 -0400

    sort topics by their number of votes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,7 @@ class TopicsController < ApplicationController
   # GET /topics.json
   def index
     @topics = Topic.all
+    @topics = @topics.sort_by{ |topic| topic.votes.count }.reverse
   end
 
   # GET /topics/1
